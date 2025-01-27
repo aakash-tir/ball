903899a4ec99ef4563f9e4da9629008304603ad1
aakash-tir <aakashtirathdas@gmail.com>
2025-01-27 13:21:24 -0800

    added a jump function to the ball

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,15 +3,20 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour{
 
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> Jump = new UnityEvent<Vector3>();
+    private bool isGrounded=false;
 
 
     // Update is called once per frame
     void Update()
     {
         Vector2 inputVector = Vector2.zero;
+        Vector3 inputVector3 = Vector3.zero;
+        
 
         if (Input.GetKey(KeyCode.W)){
             inputVector += Vector2.up;
+            
         }
         if (Input.GetKey(KeyCode.A)){
             inputVector += Vector2.left;
@@ -22,6 +27,32 @@ public class InputManager : MonoBehaviour{
         if (Input.GetKey(KeyCode.D)){
             inputVector += Vector2.right;
         }
+        if (Input.GetKey(KeyCode.Space) && isGrounded ){
+            inputVector3+=Vector3.up;
+            isGrounded=false;
+            
+        }
+        
         OnMove?.Invoke(inputVector);
+        Jump?.Invoke(inputVector3);
     }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true; 
+        }
+    }
+
+  
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = false; 
+        }
+    }
+
 }
--- Assets/ballController.cs
+++ Assets/ballController.cs
@@ -4,11 +4,21 @@ public class ballController : MonoBehaviour{
 
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField]  float ballSpeed;
+    [SerializeField]  float ballJumpMultiplier;
 
 
     public void MoveBall(Vector2 input){
         Vector3 inputXZPlane = new(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
+
+    public void onJump(Vector3 input){
+        sphereRigidbody.AddForce(input * ballJumpMultiplier,ForceMode.Impulse);
+        
+    }
+
+    
+
+
 
 }
